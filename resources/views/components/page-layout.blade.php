c0fa1373bd9d8f9d8d906e60f9a5b0c0cb6a2809
Oleksandr Kovalchuk <sanyol255@gmail.com>
2023-03-07 15:13:10 +0200

    Creating form

---
+++ resources/views/components/page-layout.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather&display=swap" rel="stylesheet">
+    @vite(['resources/scss/app.scss', 'resources/js/app.js'])
+</head>
+<body class="text-center mt-3   ">
+    {{ $slot }}
+</body>
+</html>
